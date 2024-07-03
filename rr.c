e9022cbe3707f475fea5b6ed7bfa4f1a45055439
Putu Nadya Putri Astina <astina.2308561046@student.unud.ac.id>
2024-07-03 08:30:11 +0800

    Adding avg waiting time & turnaround time

--- rr.c
+++ rr.c
@@ -7,6 +7,7 @@ int main()
     int pid[100], at[100], bt[100], ct[100], tat[100], wt[100], rt[100], remaining_bt[100];
     int current_time = 0, completed = 0;
     int is_completed[100] = {0}; 
+    float total_wt = 0, total_tat = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
@@ -92,9 +93,58 @@ int main()
         }
     }
 
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (pid[j] > pid[j + 1])
+            {
+                int temp = pid[j];
+                pid[j] = pid[j + 1];
+                pid[j + 1] = temp;
+
+                temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
+
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = ct[j];
+                ct[j] = ct[j + 1];
+                ct[j + 1] = temp;
+
+                temp = tat[j];
+                tat[j] = tat[j + 1];
+                tat[j + 1] = temp;
+
+                temp = wt[j];
+                wt[j] = wt[j + 1];
+                wt[j + 1] = temp;
+
+                temp = rt[j];
+                rt[j] = rt[j + 1];
+                rt[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    float avg_wt = total_wt / n;
+    float avg_tat = total_tat / n;
+
     printf("PID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
     for (int i = 0; i < n; i++)
         printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", pid[i], at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
+
+    printf("Average Waiting Time: %.2f\n", avg_wt);
+    printf("Average Turnaround Time: %.2f\n", avg_tat);
 
     return 0;
 }
